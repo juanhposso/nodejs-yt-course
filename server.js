2afd2ce0497ed4cbf5660d1c2e984b78572a5ca2
juanhposso <juan.hernandez0793@gmail.com>
2021-09-25 02:24:29 -0500

    server con nodeJS puro terminado

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 	// set header content type
-	//res.setHeader('content-type', 'text/plain');
-	res.write('hello juan');
-	res.end();
+	res.setHeader('content-type', 'text/html');
+
+	let path = '/views/';
+
+	switch (req.url) {
+		case '/':
+			path += 'index.html';
+			res.statusCode = 200;
+			break;
+		case '/about':
+			path += 'about.html';
+			res.statusCode = 200;
+
+			break;
+		case '/about-me':
+			res.statusCode = 301;
+			res.setHeader('Location', '/about');
+			res.end();
+
+			break;
+		default:
+			path += 'error404.html';
+			res.statusCode = 404;
+			break;
+	}
+
+	// send HTML file
+	fs.readFile(__dirname + path, (err, data) => {
+		if (err) {
+			console.error(err);
+			res.end();
+		} else {
+			res.end(data);
+		}
+	});
 });
 
 server.listen(3000, () => {
