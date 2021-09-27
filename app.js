2d070f9eccb745f3ccf8c23b1eb8ec7274652f8a
juanhposso <juan.hernandez0793@gmail.com>
2021-09-26 23:34:27 -0500

    servidor funcionando y corriendo con express sin view engine

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
 
+// * register view engine
+app.set('view engine', 'ejs');
+
+// * lister for a request
 app.listen(3000);
 
+// * Request main index
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/views/index.html');
 });
 
+// * request /about
 app.get('/about', (req, res) => {
 	res.sendFile(__dirname + '/views/about.html');
+});
+
+// * Redirects
+app.get('/about-us', (req, res) => {
+	res.redirect('/about');
+});
+
+// * 404
+app.use((req, res) => {
+	res.status(404).sendFile(__dirname + '/views/error404.html');
 });
